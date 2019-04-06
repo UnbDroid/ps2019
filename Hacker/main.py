e91c6f261082ea30ae7e4ee22f7b1006ccecbec5
rafael-at97 <31388822+rafael-at97@users.noreply.github.com>
2019-04-06 12:34:36 -0300

    Atualizado código botões hacker

--- Hacker/main.py
+++ Hacker/main.py
@@ -1,105 +1,124 @@
-#from gpiozero import Button
-
 import RPi.GPIO as GPIO
-from time import sleep
+import time
+from os import system
 
-#b1 = Button(7, pull_up=False)
-#b2 = Button(11, pull_up=False)
-#b3 = Button(13, pull_up=False)
-#b4 = Button(15, pull_up=False)
+botao_0 = 37
+botao_1 = 35
+botao_2 = 33
+botao_3 = 31
+botao_4 = 29
+botao_5 = 40
+botao_6 = 38
+botao_7 = 36
 
 GPIO.setmode(GPIO.BOARD)
 
 x = 0
 
-GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(29, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#clear = lambda : system('clear')
 
+GPIO.setup(botao_0, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(botao_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(botao_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(botao_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(botao_4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(botao_5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(botao_6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(botao_7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+sequence = [botao_5, botao_3, botao_7, botao_4, botao_1, botao_2, botao_6, botao_0]
+
+changed = True
+
+def not_right(x, i):
+	if(i<x):
+		return sequence[i]
+	else:
+		return sequence[i+1]	
+
+system('clear')
+print("HAHAHA you didn't say the magic word...")
 
 while x<8:
-
+	if changed is True:
+		system('clear')
+		changed = False
+		print("HAHAHA you didn't say the magic word...")
+							
 	if x == 0:
-		if GPIO.input(31) == 1:
-			print("Primeiro botao certo")
+		if GPIO.input(sequence[x]) == 1:
+			system('clear')
+			print("Voce nunca conseguira descobrir a sequencia correta...")
 			x = 1
-			sleep(0.4)
+			time.sleep(4)
+			changed = True
 	if x == 1:
-		if GPIO.input(33) == 1:
-			print("Segundo botao certo")
+		if GPIO.input(sequence[x]) == 1:
+			system('clear')
+			print("Suas tentativas sao em vao...")
 			x = 2
-			sleep(0.4)
-		elif GPIO.input(31) or GPIO.input(35) or GPIO.input(37) or GPIO.input(29) or GPIO.input(40) or GPIO.input(38) or GPIO.input(36):
-			print("Achou errado, otario")
-			print(" ")
+			time.sleep(4)
+			changed = True
+		elif GPIO.input(not_right(x, 0)) or GPIO.input(not_right(x, 1)) or GPIO.input(not_right(x, 2)) or GPIO.input(not_right(x, 3)) or GPIO.input(not_right(x, 4)) or GPIO.input(not_right(x, 5)) or GPIO.input(not_right(x, 6)):
 			x = 0
-			sleep(0.4)
+			time.sleep(0.4)
 	if x == 2:
-		if GPIO.input(35) == 1:
-			print("Terceiro botao certo")
+		if GPIO.input(sequence[x]) == 1:
+			system('clear')
+			print("Ta frio...")
 			x = 3
-			sleep(0.4)
-		elif GPIO.input(31) or GPIO.input(33) or GPIO.input(37) or GPIO.input(29) or GPIO.input(40) or GPIO.input(38) or GPIO.input(36):
-			print("Errrrroooou")
-			print(" ")
+			time.sleep(4)
+			changed = True
+		elif GPIO.input(not_right(x, 0)) or GPIO.input(not_right(x, 1)) or GPIO.input(not_right(x, 2)) or GPIO.input(not_right(x, 3)) or GPIO.input(not_right(x, 4)) or GPIO.input(not_right(x, 5)) or GPIO.input(not_right(x, 6)):
 			x = 0
-			sleep(0.4)
+			time.sleep(0.4)
 	if x == 3:
-		if GPIO.input(37) == 1:
-			print("Quarto botao certo")
+		if GPIO.input(sequence[x]) == 1:
+			system('clear')
+			print("Voces vao mesmo deixar o foguete explodir?")
 			x = 4
-			sleep(0.4)
-		elif GPIO.input(31) or GPIO.input(33) or GPIO.input(35) or GPIO.input(29) or GPIO.input(40) or GPIO.input(38) or GPIO.input(36):
-			print("Voltou pra estaca zero")
-			print(" ")
+			time.sleep(4)
+			changed = True
+		elif GPIO.input(not_right(x, 0)) or GPIO.input(not_right(x, 1)) or GPIO.input(not_right(x, 2)) or GPIO.input(not_right(x, 3)) or GPIO.input(not_right(x, 4)) or GPIO.input(not_right(x, 5)) or GPIO.input(not_right(x, 6)):
 			x = 0
-			sleep(0.4)
+			time.sleep(0.4)
 	if x == 4:
-		if GPIO.input(29) == 1:
-			print("Quinto botao certo")
+		if GPIO.input(sequence[x]) == 1:
+			system('clear')
+			print("As mentes brilhantes do Brasil nao sao de nada...")
 			x = 5
-			sleep(0.4)
-		elif GPIO.input(31) or GPIO.input(33) or GPIO.input(35) or GPIO.input(37) or GPIO.input(40) or GPIO.input(38) or GPIO.input(36):
-			print("E la se foi...")
-			print(" ")
+			time.sleep(4)
+			changed = True
+		elif GPIO.input(not_right(x, 0)) or GPIO.input(not_right(x, 1)) or GPIO.input(not_right(x, 2)) or GPIO.input(not_right(x, 3)) or GPIO.input(not_right(x, 4)) or GPIO.input(not_right(x, 5)) or GPIO.input(not_right(x, 6)):
 			x = 0
-			sleep(0.4)
+			time.sleep(0.4)
 	if x == 5:
-		if GPIO.input(40) == 1:
-			print("Sexto botao certo")
+		if GPIO.input(sequence[x]) == 1:
+			system('clear')
+			print("Pare de tentar isso...")
 			x = 6
-			sleep(0.4)
-		elif GPIO.input(31) or GPIO.input(33) or GPIO.input(35) or GPIO.input(37) or GPIO.input(29) or GPIO.input(38) or GPIO.input(36):
-			print("Tururuuuu")
-			print(" ")
+			time.sleep(4)
+			changed = True
+		elif GPIO.input(not_right(x, 0)) or GPIO.input(not_right(x, 1)) or GPIO.input(not_right(x, 2)) or GPIO.input(not_right(x, 3)) or GPIO.input(not_right(x, 4)) or GPIO.input(not_right(x, 5)) or GPIO.input(not_right(x, 6)):
 			x = 0
-			sleep(0.4)
+			time.sleep(0.4)
 	if x == 6:
-		if GPIO.input(38) == 1:
-			print("Setimo botao certo")
+		if GPIO.input(sequence[x]) == 1:
+			system('clear')
+			print("Se voce errar agora todo mundo morrera!")
 			x = 7
-			sleep(0.4)
-		elif GPIO.input(31) or GPIO.input(33) or GPIO.input(35) or GPIO.input(37) or GPIO.input(40) or GPIO.input(29) or GPIO.input(36):
-			print("Tu errou aqui mano? Serio?")
-			print(" ")
+			time.sleep(4)
+			changed = True
+		elif GPIO.input(not_right(x, 0)) or GPIO.input(not_right(x, 1)) or GPIO.input(not_right(x, 2)) or GPIO.input(not_right(x, 3)) or GPIO.input(not_right(x, 4)) or GPIO.input(not_right(x, 5)) or GPIO.input(not_right(x, 6)):
 			x = 0
-			sleep(0.4)
+			time.sleep(0.4)
 	if x == 7:
-		if GPIO.input(36) == 1:
-			print("ACERTOOOOU MIZERAAAAVIIII")
+		if GPIO.input(sequence[x]) == 1:
+			system('clear')
+			print("Aaaaarrghh, voce passou por isso, mas o foguete ainda explodira...")
 			x = 8
-			sleep(0.4)
-		elif GPIO.input(31) or GPIO.input(33) or GPIO.input(35) or GPIO.input(40) or GPIO.input(29) or GPIO.input(40) or GPIO.input(38):
-			print("Errou no ulllttiimoooooo!!!!")
-			print(" ")
+			time.sleep(5)
+			changed = True
+		elif GPIO.input(not_right(x, 0)) or GPIO.input(not_right(x, 1)) or GPIO.input(not_right(x, 2)) or GPIO.input(not_right(x, 3)) or GPIO.input(not_right(x, 4)) or GPIO.input(not_right(x, 5)) or GPIO.input(not_right(x, 6)):
 			x = 0
-			sleep(0.4)
-
-# GPIO.output(17, 1)  # turn on pin 17
-# GPIO.output(18, 1)  # turn on pin 18
+			time.sleep(0.4)
